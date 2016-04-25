0a1501f71628d841d0837c8b71e0d07aadc8c3b0
nev <pi.wiacek@gmail.com>
2016-04-25 23:26:18 +0200

    Small fix for problems with running PyGue in some terminals - the map is scaled down for the time being while the error is being examined.

--- data/location/template/test_location.py
+++ data/location/template/test_location.py
@@ -1,7 +1,8 @@
 from machine.tile import MetaTile
 
 # create example grid
-tiles_ = [[MetaTile(i, j) for j in range(20)] for i in range(32)]
+# tiles_ = [[MetaTile(i, j) for j in range(20)] for i in range(32)]
+tiles_ = [[MetaTile(i, j) for j in range(10)] for i in range(15)]
 
 from machine.wall import *
 from machine.door import *
